8622c12b63795b3bf9420ce612c1ed2c4b34b920
testcentricity <testcentricity@gmail.com>
2018-04-08 09:37:08 -0700

    Added exclusionary hooks and HTML report header

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,6 +1,20 @@
 
+def run_first_once
+  # HTML report header information if reporting is enabled
+  if ENV['REPORTING']
+    report_header = "\n<b><u>TEST ENVIRONMENT</u>:</b> #{ENV['TEST_ENVIRONMENT']}\n"\
+      "\n  <b>Browser:</b>\t #{Environ.browser.capitalize}\n"
+    report_header = "#{report_header}  <b>Device:</b>\t\t #{Environ.device_name}\n" if Environ.device_name
+    report_header = "#{report_header}  <b>Device o/s:</b>\t #{Environ.device_os}\n" if Environ.device_os
+    report_header = "#{report_header}  <b>Device type:</b>\t #{Environ.device_type}\n" if Environ.device_type
+    report_header = "#{report_header}\n\n"
+    puts report_header
+  end
+end
+
 
 Before do |scenario|
+  # if executing tests in parallel concurrent threads, print thread number with scenario name
   ENV['PARALLEL'] ?
       message = "Thread ##{ENV['TEST_ENV_NUMBER']} | Scenario:  #{scenario.name}" :
       message = "Scenario:  #{scenario.name}"
@@ -15,12 +29,11 @@ end
 
 
 
-# exclusionary around hooks to prevent running feature/scenario on unsupported browser, devices, or
+# exclusionary Around hooks to prevent running feature/scenario on unsupported browsers, devices, or
 # cloud remote browser hosting platforms. Use the following tags to block test execution:
 #   desktop web browsers:      @!safari, @!ie, @!firefox, @!chrome, @!edge
 #   mobile devices:            @!ipad, @!iphone
 #   remotely hosted browsers:  @!browserstack, @!saucelabs
-
 
 # block feature/scenario execution if running against Safari browser
 Around('@!safari') do |scenario, block|
@@ -66,13 +79,23 @@ end
 
 # block feature/scenario execution if running against Browserstack cloud-hosted service
 Around('@!browserstack') do |scenario, block|
-  qualify_browser(:browserstack, 'BrowserStack', scenario, block)
+  if Capybara.default_driver != :browserstack
+    block.call
+  else
+    puts "Scenario '#{scenario.name}' cannot be executed with the BrowserStack service."
+    skip_this_scenario
+  end
 end
 
 
 # block feature/scenario execution if running against Sauce Labs cloud-hosted service
 Around('@!saucelabs') do |scenario, block|
-  qualify_browser(:saucelabs, 'SauceLabs', scenario, block)
+  if Capybara.default_driver != :saucelabs
+    block.call
+  else
+    puts "Scenario '#{scenario.name}' cannot be executed with the SauceLabs service."
+    skip_this_scenario
+  end
 end
 
 
@@ -83,6 +106,26 @@ Around('@!device') do |scenario, block|
     skip_this_scenario
   else
     block.call
+  end
+end
+
+
+Around('@!ios') do |scenario, block|
+  if Environ.device_os == :android
+    block.call
+  else
+    puts "Scenario '#{scenario.name}' can not be executed on iOS devices."
+    skip_this_scenario
+  end
+end
+
+
+Around('@!android') do |scenario, block|
+  if Environ.device_os == :ios
+    block.call
+  else
+    puts "Scenario '#{scenario.name}' can not be executed on Android devices."
+    skip_this_scenario
   end
 end
 
